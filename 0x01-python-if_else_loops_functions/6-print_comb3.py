826603a626eca1e09ef2f538dc6a6028c1646ad0
Irene277 <tonskiey2k@yahoo.com>
2022-09-20 19:58:05 -0700

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for digit in range(89):    
+for digit in range(89):
     if digit / 10 < digit % 10:
         print("{:02d}".format(digit), end=", ")
 print("{:02d}".format(digit+1))
